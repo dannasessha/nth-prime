fe8f21f523e781d4c848e7bf146af5de5bb0fc7c
dannasessha <sessha@zingolabs.com>
2020-10-08 10:02:18 -0600

    refactored with & + * on collection and sqrt optimization

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,26 @@
 pub fn nth(n: u32) -> u32 {
-	const UPPER_BOUND: usize = 1000002;
-
-//make prime list
-// ---boolean array collection---
-
-	//set array of upper_bound length to all true.
-	let mut numbers: [bool; UPPER_BOUND] = [true; UPPER_BOUND];
-
-		// create index_count which will work as representations of the numbers to be tested.
-		let mut index_count: usize = 0;
-
-		// test each index_count (number) for primality
-		while index_count < UPPER_BOUND {
-
-			// make factor variable, beginning with 2
-			let mut factor: usize = 2;
-			// test while factor variable is less than square root of number to be tested, to avoid repition (eg 2*4 = 8 and 4*2 = 8)
-			while factor as f32 <= (index_count as f32).sqrt(){
-
-				//this is the crashing macro ---->dbg!(factor);
-				//if index is not prime... 
-				if index_count % factor == 0 {
-					//set array value to false at this index.
-					numbers[index_count] = false;
-					// then break
-					break
-				}
-				factor = factor + 1
-			}
-			index_count = index_count + 1
-		}
-
-//find nth prime 
-	//create empty Vec for pushing primes into.
-	let mut accumulated_primes = Vec::new();
-	// make prime_query_index variable, starting with 2 (to avoid including 0 and 1, which are not prime)
-	let mut prime_query_index: usize = 2;
-	// check entire range of tested numbers
-	while prime_query_index < UPPER_BOUND {
-		// if number previously was not converted to false by test above
-		if numbers[prime_query_index] == true {
-			//push the index (synchronized with prime_query_index) into accumulated_primes
-			accumulated_primes.push(prime_query_index as u32);
-		}
-		prime_query_index = prime_query_index + 1
-	} 
-
-// return requested prime by index provided as parameter
-accumulated_primes[n as usize]
-
+    const UPPER_BOUND: u32 = 1000003;
+    let mut integers = Vec::new();
+    let mut count: u32 = 3;
+    while count <= UPPER_BOUND {
+        integers.push(count);
+        count = count + 1
+    }
+    let mut prime_numbers: Vec<u32> = Vec::new();
+    prime_numbers.push(2);
+    for num in integers {
+        let test_range = (num as f32).sqrt() as u32;
+        for prime in &prime_numbers {
+            let test = *prime;
+            if test <= test_range {
+                if num % prime == 0 {
+                    break;
+                }
+            } else if test > test_range {
+                prime_numbers.push(num);
+                break;
+            }
+        }
+    }
+    prime_numbers[n as usize]
 }
